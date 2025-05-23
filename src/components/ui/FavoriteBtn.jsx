bc03a116281a6f3dd441a0d2aea00fb06c24b322
Bianca Santos <biancassantos89@gmail.com>
2025-05-23 20:27:29 -0300

    favorite button propagation bug fixed

--- src/components/ui/FavoriteBtn.jsx
+++ src/components/ui/FavoriteBtn.jsx
@@ -4,11 +4,16 @@ import { toggleFavorite } from "../../store/logsSlice";
 
 const FavoriteBtn = ({ isFavorite, id }) => {
 	const dispatch = useDispatch();
+	const onClick = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		dispatch(toggleFavorite(id));
+	}
 
 	return (
 		<button
 			type="button"
-			onClick={() => dispatch(toggleFavorite(id))}
+			onClick={e => onClick(e)}
 			className="hover:bg-dark-gray transition-colors duration-300 p-1 rounded text-xl z-50"
 		>
 			{isFavorite === true ? <BsStarFill /> : <BsStar />}
